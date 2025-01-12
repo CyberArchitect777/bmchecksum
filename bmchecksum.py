392fb0884066d7da68234d25bf22926f0e3b2e49
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2025-01-12 18:43:42 +0000

    feat: create initial command-line interface

--- bmchecksum.py
+++ bmchecksum.py
@@ -15,3 +15,50 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
+import sys
+
+def help():
+    
+    """
+    Outputs help information to the user if asked for or if an invalid function is specified
+    """
+    
+    print("General usage:")
+    print("\nbmchecksum <command> <base directory>")
+    print("\nCommands:")
+    print("\n-c = Create checksums for all subdirectories in the base directory")
+    print("-v = Verify file checksums in all subdirectories based on those found in the base directory")
+    print("-h = Help\n")
+
+def main():
+    
+    """
+    The first function run upon program start
+    """
+    
+    print("\nBMChecksum")
+    print("By Barrie Millar")
+    print("A file hashing program to store and later verify the checksums of files\n")
+
+    if len(sys.argv) == 1:
+        help()
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        command = sys.argv[1]
+        if command == "-h":
+            help()
+            sys.exit(1)
+        elif command == "-c":
+            print("Please provide a base directory name to calculate checksums on\n")
+    else:
+        command = sys.argv[1]
+        base_directory = sys.argv[2]
+
+if __name__ == "__main__":
+    
+    """
+    Runs the main function if this code is being run directly.
+    """
+    
+    main()
